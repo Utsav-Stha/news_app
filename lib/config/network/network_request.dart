436c15fd9a110bcdadc0696784b0ff6dc37caa72
Utsav Shrestha <shrestha8502@gmail.com>
2023-10-13 21:23:15 +0545

    Fetched data over API and displayed

--- lib/config/network/network_request.dart
+++ lib/config/network/network_request.dart
@@ -1,13 +1,25 @@
-
 import 'package:dio/dio.dart';
+import 'package:news_app/feature/tech_crunch_news/technews_model.dart';
 
-class NetworkRequest{
-  static String techNews = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=d043b06ae6ce4a6ca8b9ec6425b24d94";
+class NetworkRequest {
+  static String techNews =
 
-  static final dio  = Dio();
-  static void getTechCrunchNews() async{
-     final response =  await dio.get(techNews);
-     print("My response :  ${response.data}");
+  "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=d043b06ae6ce4a6ca8b9ec6425b24d94";
+
+  static final dio = Dio();
+  static Future<List<Articles>?> getTechCrunchNews() async {
+    final response = await dio.get(techNews);
+    // print("My response :  ${response.data}");
+
+    print("Status Code: ${response.statusCode}");
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      //do this things
+      //here TechnewsModel ma json ko articles ko data matra jnxa esri
+      //also check greko if empty hunxa ki nai if bho bhne empty list pathako
+       return TechnewsModel.fromJson(response.data).articles ?? [];
+    }
+    //if condn satisfy bhyena bhne matra tala ko return huanxa
+    return [];
   }
-
 }
--- lib/feature/tech_crunch_news/tech_crunch_news.dart
+++ lib/feature/tech_crunch_news/tech_crunch_news.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/feature/tech_crunch_news/technews_model.dart';
 
 import '../../config/network/network_request.dart';
 
@@ -12,14 +13,79 @@ class TechCrunchNews extends StatefulWidget {
 class _TechCrunchNewsState extends State<TechCrunchNews> {
   @override
   Widget build(BuildContext context) {
-
-    NetworkRequest.getTechCrunchNews();
-
     return Scaffold(
-      body: ListView(
-        children: [
-          Text('TEch News'),
-        ],
+      appBar: AppBar(
+        title: Text('TechNews'),
+      ),
+      body: RefreshIndicator(
+        onRefresh: ()async{
+          NetworkRequest.getTechCrunchNews();
+        },
+        child: ListView(
+          shrinkWrap: true,
+          children: [
+            FutureBuilder(
+              //future ma future wala task ho jun time lagxa
+              future: NetworkRequest.getTechCrunchNews(),
+              //snapshot bhneko current data mageko, actual data chai snapshot ma auxa
+              builder: (context, snapshot) {
+                // suruma connection check grne
+                if (snapshot.hasData) {
+
+                  return ListView.separated(
+                    physics: NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                      itemCount: snapshot.data?.length ?? 0,
+                      itemBuilder: (context,index){
+                      var news = snapshot.data?[index];
+
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Card(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                children: [
+                                  Text(news?.title ?? ''),
+                                  SizedBox(
+                                    height: 8,
+                                  ),
+                                  Image.network(news?.urlToImage ?? ''),
+                                  SizedBox(
+                                    height: 8,
+                                  ),
+                                  Text(news?.description ?? ''),
+                                  SizedBox(
+                                    height: 8,
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(news?.author ?? ''),
+                                      Text(news?.publishedAt ?? ''),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      }, separatorBuilder: (BuildContext context, int index) { return Divider(); },);
+                } else if (snapshot.hasError) {
+                  return Center(
+                    child: Text(
+                      "Error : ${snapshot.error}",
+                    ),
+                  );
+                } else {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              },
+            )
+          ],
+        ),
       ),
     );
   }
