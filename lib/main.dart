206baed18ce543779f718f9d4e9f0ef9ac90cb93
Utsav Shrestha <shrestha8502@gmail.com>
2024-02-18 22:33:33 +0545

    changed Starting point of app

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
         useMaterial3: true,
       ),
-      home: HomePage(),
+      home: StartingScreen(),
     );
   }
 }
